eeffb8f9be313b8496f925a01d26e0ce100ec51b
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-03-27 20:21:35 -0700

    add server

--- app/server.js
+++ app/server.js
@@ -2,3 +2,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+var app = express();
+var router = express.Router();
+
+var port = process.env.API_PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+ res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Credentials', 'true');
+ res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+ res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//and remove cacheing so we get the most recent comments
+ res.setHeader('Cache-Control', 'no-cache');
+ next();
+});
+
+router.get("/", (req, res) => {
+  res.json({ message: 'API Initialized' });
+});
+
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log('api running on port: ', port);
+});
