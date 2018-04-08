7af09f22cc976755ceed9187f42771ffaab703fc
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-04-07 19:45:47 -0700

    get successful res from google api

--- app/api/places.js
+++ app/api/places.js
@@ -3,24 +3,35 @@ const router = express.Router();
 const Place = require('../models/place');
 const { merge } = require('lodash');
 const key = 'AIzaSyAuID19sxhCthckUbYSJLihvs9daXytRag';
+const axios = require('axios');
 
 router.route('/places')
   .get((req, res) => {
+    console.log(req.query);
+
+    console.log(
+      'GET PLACES FROM GOOGLE API REQUESTED WITH QUERY: ' +
+      req.query.name,
+      "\x1b[36m"
+   );
+
     let params = {
-      name: req.body.name,
-      location: req.body.location,
-      rankby: 'prominence',
-      key
+      name: req.query.name,
+      location: req.query.location,
+      rankby: 'distance',
     }
 
     axios.get(
-      `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
-      params
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${key}`,
+      { params }
     )
     .then((response) => {
-      res.json(response);
+      console.log(response.data.results);
+      res.json(response.data.results);
+
     })
     .catch((err) => {
+      console.log(err, "\x1b[31m");
       res.send(err);
     })
   })
