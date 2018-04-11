f806f8c8af1d64607af4ba1c0ef29a178ceee32e
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-04-10 20:13:28 -0700

    asyn await

--- app/utils/getBusy.js
+++ app/utils/getBusy.js
@@ -3,46 +3,48 @@ const key = 'AIzaSyCQbp4QicSsS_PtZWRJpBPaOd5jJBY1Dy0';
 const Flag = require('./node_colors');
 const Place = require('../models/place');
 
-function getBusyHours(places) {
+async function getBusyHours(places) {
+  let placesLeft = places.length;
+
   for (var place of places) {
     console.log(Flag.red, `Getting busy hours for: ${place.name}`);
 
-    BusyHours(place.placeid, key).then((data) => {
-      console.log(Flag.green, `busyHours recieved for: ${place.name}`);
-      let dataString = JSON.stringify(data);
+    let data = await BusyHours(place.placeid, key);
 
-      Place.findById(place._id, (err, place) => {
-        console.log(place);
-        if (err) {
-          console.log(Flag.red, err);
-          return;
-        }
+    console.log(Flag.green, `busyHours recieved for: ${place.name}`);
+    let dataString = JSON.stringify(data);
 
-        console.log(place);
+    let query = await Place.findById(place._id);
 
-        place['busyHours'] = dataString;
-        place.save((err) => {
-          if (err)
-          console.log(Flag.red, err);;
-        })
-      })
-
-      console.log(Flag.green, `busyHours added to: ${place.name}`);
+    query['busyHours'] = dataString;
+    query.save((err) => {
+      if (err)
+      console.log(Flag.red, err);;
     })
-  }
 
+    console.log(Flag.green, `busyHours added to: ${query.name}`);
+  }
 }
 //
-// getBusyHours([
-//     {
-//         "_id": "5acb0152c7ebdd2e342d6444",
-//         "placeid": "ChIJNZoFtUd-j4ARJedNpFz-mow",
-//         "name": "Philz Coffee",
-//         "isOpen": false,
-//         "address": "3101 24th St SF, CA 94110",
-//         "phoneNumber": "415-875-9370",
-//         "__v": 0
-//     }
-// ]);
+getBusyHours([
+    {
+        "_id": "5acd46ca99d6eb498c0a9507",
+        "placeid": "ChIJNZoFtUd-j4ARJedNpFz-mow",
+        "name": "Philz Coffee",
+        "isOpen": true,
+        "address": "3101 24th St SF, CA 94110",
+        "phoneNumber": "415-875-9370",
+        "__v": 0
+    },
+    {
+        "_id": "5acd4b1310c1394a24ed9f28",
+        "placeid": "ChIJiwOc1iJ-j4ARmtSY2tM29G0",
+        "name": "Mau",
+        "isOpen": false,
+        "address": "665 Valencia St SF, CA 94110",
+        "phoneNumber": "415-934-8889",
+        "__v": 0
+    }
+]);
 //
 module.exports = getBusyHours;
