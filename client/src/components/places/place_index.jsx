1740159dd611ced586063b0305787f4d7f5c498e
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-04-26 00:33:16 -0700

    add styling

--- client/src/components/places/place_index.jsx
+++ client/src/components/places/place_index.jsx
@@ -67,7 +67,11 @@ class PlaceIndex extends React.Component {
   }
 
   render() {
-
+    if (typeof this.places === 'object') {
+      return (
+        <a>Loading ...</a>
+      )
+    }
     return (
           <SideBar
             sidebar={ (
--- client/src/components/places/place_index_item.jsx
+++ client/src/components/places/place_index_item.jsx
@@ -1,8 +1,18 @@
-
-
 import React from 'react';
 import PlaceItemOptions from './place_item_options';
 import Draggable from 'react-draggable';
+
+// formatAMPM from => https://stackoverflow.com/a/8888498
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
 
 class PlaceIndexItem extends React.Component {
   constructor(props) {
@@ -38,30 +48,42 @@ class PlaceIndexItem extends React.Component {
   }
 
   renderText(isClosed) {
-    if (this.state.mouseStatus === 'entered') {
+    let lastUpdated = this.props.place.lastUpdated;
+    let dateObj = new Date(lastUpdated);
+    let time = formatAMPM(dateObj);
+    let date = dateObj.toDateString();
+
+    if (this.state.mouseStatus === 'entered' && lastUpdated) {
       return (
-        this.props.place.lastUpdated
+        <div id='last-updated'>
+          <a>{'last updated: '}</a>
+          <a>{date}</a>
+          <a>{time}</a>
+          <a>Click to refresh.</a>
+        </div>
       )
     } else {
       return (
-        <div>
-          <div id='place-item-header'>
-            <a id='place-name'>{ this.props.place.name }</a>
-            <a
-              id='delete-this.props.place-button'
-              onClick={() => this.props.deleteFavorite(this.props.place._id)}
-              >
-              X
-            </a>
-          </div>
+        <div id='place-item-initial'>
           <a>{ this.props.place.address }</a>
 
-          <a>
+          <a id='percent-busy'>
             {
               isClosed ? '' :
-              this.props.place.busyPercentage ?
-              this.props.place.busyPercentage + '% busy' :
-              'getting percentage ...'
+              this.props.place.busyPercentage
+              ? this.props.place.busyPercentage + '% busy'
+              : 'Click to see busyPercentage'
+            }
+          </a>
+          <a>
+            {
+              isClosed ? 'CLOSED' : (
+                <div
+                  id='percentage'
+                  style={this.state.style}
+                  >
+                </div>
+            )
             }
           </a>
       </div>
@@ -88,16 +110,22 @@ class PlaceIndexItem extends React.Component {
           onMouseEnter={() => this.handleHover('entered')}
           onMouseLeave={() => this.handleHover('left')}
           >
+          <div id='place-item-header'>
+            <a
+              id='delete-this.props.place-button'
+              onClick={() => this.props.deleteFavorite(this.props.place._id)}
+              >
+              X
+            </a>
+            <a id='place-name'>{
+                this.props.place.name.length > 12
+                ? this.props.place.name.slice(0, 12) + ' ...'
+                : this.props.place.name
+              }</a>
+          </div>
           {
             this.renderText(isClosed)
           }
-          { isClosed ? 'CLOSED' : (
-            <div
-              id='percentage'
-              style={this.state.style}
-              >
-            </div>
-        )}
         </div>
     )
   }
