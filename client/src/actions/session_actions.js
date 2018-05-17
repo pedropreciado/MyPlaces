f2f0a0caa682f24ce0202c6de3905e659d012e56
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-05-16 19:08:36 -0700

    remove local user on logout && build before heroku ppush

--- client/src/actions/session_actions.js
+++ client/src/actions/session_actions.js
@@ -17,46 +17,50 @@ export const login = (user) => dispatch => {
   SessionAPIUtil.login(user)
     .then(({ data }) => {
       if (data.errmsg) {
-        dispatch(receiveErrors(data.errmsg))
+        dispatch(receiveErrors(data.errmsg));
       } else {
-        dispatch(receiveCurrentUser(data))
+        dispatch(receiveCurrentUser(data));
 
         window.localStorage.setItem('user', JSON.stringify(data));
       }
   })
     .catch((err) => {
       dispatch(receiveErrors(err));
-    })
-}
+    });
+};
 
 export const logout = () => dispatch => {
   SessionAPIUtil.logout()
-    .then(dispatch(receiveCurrentUser(null)))
-    .catch((err) => {
-      dispatch(receiveErrors(err))
+    .then(() => {
+      window.localStorage.clear();
+
+      dispatch(receiveCurrentUser(null));
     })
-}
+    .catch((err) => {
+      dispatch(receiveErrors(err));
+    });
+};
 
 export const signup = (user) => dispatch => {
   SessionAPIUtil.signup(user)
     .then(({ data }) => {
       if (data.errmsg) {
-        dispatch(receiveErrors(data.errmsg))
+        dispatch(receiveErrors(data.errmsg));
       } else {
-        dispatch(receiveCurrentUser(data))
+        dispatch(receiveCurrentUser(data));
       }
     })
     .catch((err) => {
       console.error(err);
-    })
-}
+    });
+};
 
 const receiveCurrentUser = (user) => ({
   type: 'RECEIVE_CURRENT_USER',
   currentUser: user
-})
+});
 
 const receiveErrors = (errors) => ({
   type: RECEIVE_ERRORS,
   errors
-})
+});
